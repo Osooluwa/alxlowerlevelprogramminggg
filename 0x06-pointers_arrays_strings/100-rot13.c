d59243116ec311a8706f8b665532c20df8f3f433
Osooluwa <osooluwaajibola@gmail.com>
2022-09-22 15:16:05 -0700

    rot

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,21 +9,21 @@
 char *rot13(char *s)
 {
 	int i, j;
-		
-		char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-		char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-		for (i = 0; s[i] != '\0'; i++)
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			for (j = 0; a[j] != '\0'; j++)
+			if (s[i] == a[j])
 			{
-				if (s[i] == a[j])
-				{
-					s[i] = b[j];
-					break;
-				}
+				s[i] = b[j];
+				break;
 			}
 		}
+	}
 
-		return (s);
+	return (s);
 }
